ca6ae1c889381d8b2400ce664275d1702bcc59ee
caoyao <caoyao@testbird.com>
2019-12-10 14:08:19 +0800

    change img

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="footer text_center">
+        <div class="white font_size_15 margin_bottom_1">
+            birdlee@birdlink.im 
+        </div>
         <img src="../../static/img/white.png" style="width: 150px" alt="">
+        <div class="white font_size_15 margin_top_10">
+            香港九龍新蒲崗六合 29 號宏輝工業大廈 2 樓 3 號
+        </div>
     </div>
 </template>
 
